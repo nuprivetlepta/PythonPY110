36da737d097d94bda42e2bb13f9acc012568d05d
nuprivetepta <stopkran510@gmail.com>
2022-04-20 21:03:57 +0300

    Выполнена работа лабораторная. 1.3 и 4.2 в практике- на дом

--- Занятие1/Домашние_задания/task1_1/main.py
+++ Занятие1/Домашние_задания/task1_1/main.py
@@ -1,3 +1,14 @@
+def zippa( keys: list) -> list:
+    nums = [i for i in range(len(keys))]
+    for num, key in zip(nums, keys):
+        print(num, key)
+
+
+
+
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+
+    list_ = ["a", "b", "c"]
+    zippa(list_)
